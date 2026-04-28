0931cbe4da395054889ec94c154a1dc39a6cff62
Jesús <jechual0704@gmail.com>
2026-04-28 07:56:34 -0600

    Ejercicio procesador de comandos

--- practica_01/procesador_comandos.py
+++ practica_01/procesador_comandos.py
@@ -1,22 +1,37 @@
 import datetime
 
 def obtener_saludo(nombre_bot):
+    """
+    Retorna un saludo formateado
+    """
     print("Hola, soy", nombre_bot, ",¿en qué puedo ayudarte?")
 
-def procesar_comando_recordar(argumento):
-    print("")
+def procesar_comando_recordar(comando):
+    """
+    Valida y procesa la acción de rocordad un dato
+    """
+    if not comando:
+        return "Error: falra el nombre. Uso !recordar [nombre]"
     
+    return f"Entendido, recordaré el nombre: {comando}"
 def calcular_uptime(hora_inicio):
-    print("")
+    """
+    Calcula la diferencia de tiempo entre el inicio y el actual (Mostrar actividad del boot)
+    """
+    ahora = datetime.datetime.now()
+    diferencia = ahora - hora_inicio
+    segundos = int(diferencia.total_seconds())
+    return f"Tiempo de actividad: {segundos} segundos"
+
     
 def mostrar_ayuda():
-    print("")
+
 
 def iniciar_agente():
-    print("")
+
 
 def main():
-    obtener_saludo("AgenteBot")
+    obtener_saludo("Discordbot")
     procesar_comando_recordar()
     calcular_uptime()
     mostrar_ayuda()
