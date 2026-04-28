0fa113de9187c70bb8fe9e005b159dc461c2aa3f
Jesús <jechual0704@gmail.com>
2026-04-28 07:42:54 -0600

    Ejercicio procesador de comandos

---
+++ practica_01/procesador_comandos.py
@@ -0,0 +1,26 @@
+import datetime
+
+def obtener_saludo(nombre_bot):
+    print("Hola, soy", nombre_bot, ",¿en qué puedo ayudarte?")
+
+def procesar_comando_recordar(argumento):
+    print("")
+    
+def calcular_uptime(hora_inicio):
+    print("")
+    
+def mostrar_ayuda():
+    print("")
+
+def iniciar_agente():
+    print("")
+
+def main():
+    obtener_saludo("AgenteBot")
+    procesar_comando_recordar()
+    calcular_uptime()
+    mostrar_ayuda()
+    iniciar_agente()   
+
+if __name__ == "__main__":
+    main()
