2d00a69047deb8f28c9dca86b86e85c437f35300
Jesús <jechual0704@gmail.com>
2026-04-28 08:43:33 -0600

    Ejercicio de procesador de comandos

--- practica_01/procesador_comandos.py
+++ practica_01/procesador_comandos.py
@@ -25,17 +25,48 @@ def calcular_uptime(hora_inicio):
 
     
 def mostrar_ayuda():
+    """
+    Comandos disponibles para el usuario
+    """
+    return {
+        "Comandos disponibles: \n",
+        "!saludo - Muestra un saludo de bot \n",
+        "!recordar [nombre]:Recuerda un nombre proporcionado por el usuario \n",
+        "!uptime:Muestra el tiempo de actividad del bot \n",
+        "!ayuda:Muestra esta lista de comandos \n",
+    }
 
-
+#Función principal para probar las funciones
 def iniciar_agente():
+    nombre_bot = "Discordbot"
+    prefijo = "!"
+    hora_inicio = datetime.datetime.now()
 
+    print(f"{obtener_saludo(nombre_bot)}")
+    print("Escribe !ayuda para ver los comandos disponibles.")
+
+    ejecutando = True
+    while ejecutando:
+        entrada = input(f"[{nombre_bot}] Ingrese comando: ").strip()
+
+        if not entrada.startswith(prefijo):
+            print("Comando no reconocido.")
+            continue
+
+        partes = entrada[len(prefijo):].split(maxsplit=1)
+        comando = partes[0].lower()
+        argumento = partes[1] if len(partes) > 1 else ""
+
+        if comando == "saludo":
+            print(obtener_saludo(nombre_bot))
+        elif comando == "ayuda":
+            print(mostrar_ayuda())
+        else :
+            print("Comando no reconocido.")
 
 def main():
-    obtener_saludo("Discordbot")
-    procesar_comando_recordar()
-    calcular_uptime()
-    mostrar_ayuda()
-    iniciar_agente()   
+    iniciar_agente()
+
 
 if __name__ == "__main__":
     main()
